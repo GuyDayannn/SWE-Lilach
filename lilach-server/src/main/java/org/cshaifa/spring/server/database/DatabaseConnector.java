ca60b361a07542054e040895ab1ae6cceba7cbf2
YotamGurfinkel <yotam706@gmail.com>
2022-04-18 00:11:49 +0300

    Added basic SQL server connection classes

---
+++ lilach-server/src/main/java/org/cshaifa/spring/server/database/DatabaseConnector.java
@@ -0,0 +1,40 @@
+package org.cshaifa.spring.server.database;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+/**
+ * This class is basically just responsible for holding
+ * a static session and handling db connections
+ */
+public class DatabaseConnector {
+    private static Session session = null;
+
+    private static SessionFactory getSessionFactory() throws HibernateException {
+        Configuration configuration = new Configuration();
+        System.out.println(configuration.getProperties());
+
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+            .applySettings(configuration.getProperties())
+            .build();
+
+        return configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    public static void closeSession() {
+        if (session != null)
+            session.close();
+    }
+
+    public static Session getSession() throws HibernateException {
+        if (session != null)
+            return session;
+
+        session = getSessionFactory().openSession();
+        return session;
+    }
+}
---
+++ lilach-server/src/main/java/org/cshaifa/spring/server/database/DatabaseHandler.java
@@ -0,0 +1,9 @@
+package org.cshaifa.spring.server.database;
+
+/**
+ * This class will handle the server requests to the db
+ * like getting the catalog, updating items etc.
+ */
+public class DatabaseHandler {
+
+}
