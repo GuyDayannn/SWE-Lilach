610b1b76f59a049e50e84f5a167a3880be5f5f25
net242 <neta5128@gmail.com>
2022-04-21 15:49:23 +0300

    combined gui with database

--- lilach-client/src/main/java/org/cshaifa/spring/client/CatalogController.java
+++ lilach-client/src/main/java/org/cshaifa/spring/client/CatalogController.java
@@ -1,5 +1,6 @@
 package org.cshaifa.spring.client;
 
+import org.cshaifa.spring.entities.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
@@ -9,37 +10,51 @@ import javafx.scene.text.Text;
 import javafx.scene.image.Image;
 
 import java.io.File;
+import java.util.List;
+
+import javafx.scene.layout.HBox;
+
 
 public class CatalogController {
 
-    @FXML
-    private Pane catalogItem;
+    @FXML    private Button NewOrderButton;
+
+    @FXML    private HBox catalogItem;
+
+    @FXML    private Button flowersLeft;
+
+    @FXML    private Button flowersRight;
+
+    @FXML    private ImageView itemImage;
+
+    @FXML    private Text itemName;
+
+    @FXML    private Text itemPrice;
+
+    @FXML    private Button showItemButton;
+
+    private List<CatalogItem> catalogItems;
 
     @FXML
-    private ImageView itemImage;
-
-    @FXML
-    private Text itemName;
-
-    @FXML
-    private Text itemPrice;
-
-    @FXML
-    private Button showItemButton;
-
-    @FXML
-    void showItem(MouseEvent event) {
-        CatalogItem item = new CatalogItem(itemName, itemPrice, itemImage);
+    void showItem(MouseEvent event) { //popup item in catalog
+        Button button = (Button)  event.getSource();
+        int id = (int) Long.parseLong(button.getId());
+        CatalogItem item = catalogItems.get(id);
         App.setCurrentItemDisplayed(item);
         App.popUpLaunch(showItemButton);
     }
 
     @FXML
-    void initialize() {
+    void initialize(List<CatalogItem> catalogItems_list) {
         try {
-            File file = new File("C:\\Users\\rtole\\Desktop\\flower.jpg");
-            Image image = new Image(file.toURI().toString());
-            itemImage.setImage(image);
+            catalogItems = ClientHandler.getCatalog();
+            int i =0;
+            for(i=0; i< catalogItems.size(); i++){
+                File file = new File("E:\\myFolder\\uni\\7th semestru\\program engineering\\flower.webp");
+                Image image = new Image(file.toURI().toString());
+                itemImage.setImage(image);
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
