6aff91b341c783bf4e54b6ff3d93c025616545ed
YotamGurfinkel <yotam706@gmail.com>
2022-04-21 22:45:12 +0300

    Added images and database generation if it's empty

--- lilach-server/src/main/java/org/cshaifa/spring/server/database/DatabaseHandler.java
+++ lilach-server/src/main/java/org/cshaifa/spring/server/database/DatabaseHandler.java
@@ -1,5 +1,9 @@
 package org.cshaifa.spring.server.database;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import javax.persistence.RollbackException;
@@ -10,7 +14,6 @@ import org.cshaifa.spring.entities.CatalogItem;
 import org.cshaifa.spring.utils.Constants;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 /**
  * This class will handle the server requests to the db
@@ -18,12 +21,43 @@ import org.hibernate.Transaction;
  */
 public class DatabaseHandler {
 
+    private static List<String> getRandomOrderedImages() {
+        List<String> imagesList = new ArrayList<>();
+        for (File imageFile : new File(DatabaseHandler.class.getResource("images").getPath()).listFiles()) {
+            imagesList.add(imageFile.getAbsolutePath());
+        }
+        Collections.shuffle(imagesList);
+        return imagesList;
+    }
+
+    private static void initializeDatabaseIfEmpty() throws HibernateException {
+        Session session = DatabaseConnector.getSession();
+        session.beginTransaction();
+        List<String> imageList = getRandomOrderedImages();
+        for (int i = 0; i < imageList.size(); i++) {
+            session.save(new CatalogItem("Random flower " + i, imageList.get(i), 40));
+        }
+        try {
+            session.flush();
+            session.getTransaction().commit();
+        } catch (IllegalStateException | RollbackException e) {
+            session.getTransaction().rollback();
+            throw new HibernateException(Constants.DATABASE_ERROR);
+        }
+    }
+
     public static List<CatalogItem> getCatalog() throws HibernateException {
+        // We assume that we're getting the catalog when we first run our app
         Session session = DatabaseConnector.getSession();
         CriteriaBuilder builder = session.getCriteriaBuilder();
         CriteriaQuery<CatalogItem> query = builder.createQuery(CatalogItem.class);
         query.from(CatalogItem.class);
-        return session.createQuery(query).getResultList();
+        List<CatalogItem> catalogItems = session.createQuery(query).getResultList();
+        if (catalogItems.isEmpty()) {
+            initializeDatabaseIfEmpty();
+            catalogItems = session.createQuery(query).getResultList();
+        }
+        return catalogItems;
     }
 
     public static void updateItem(CatalogItem newItem) throws HibernateException {
@@ -31,8 +65,10 @@ public class DatabaseHandler {
         session.beginTransaction();
         session.update(newItem);
         try {
+            session.flush();
             session.getTransaction().commit();
         } catch (IllegalStateException | RollbackException e) {
+            session.getTransaction().rollback();
             throw new HibernateException(Constants.DATABASE_ERROR);
         }
     }
