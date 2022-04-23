bb897309cb387417807d49dc03620e4ba6d42d9f
YotamGurfinkel <yotam706@gmail.com>
2022-04-23 21:49:26 +0300

    Changed client port to be from Constants

--- lilach-client/src/main/java/org/cshaifa/spring/client/ClientHandler.java
+++ lilach-client/src/main/java/org/cshaifa/spring/client/ClientHandler.java
@@ -9,9 +9,10 @@ import org.cshaifa.spring.entities.requests.GetItemRequest;
 import org.cshaifa.spring.entities.requests.UpdateItemRequest;
 import org.cshaifa.spring.entities.responses.GetCatalogResponse;
 import org.cshaifa.spring.entities.responses.UpdateItemResponse;
+import org.cshaifa.spring.utils.Constants;
 
 public class ClientHandler {
-    private static LilachClient client = new LilachClient("localhost", 8080);
+    private static LilachClient client = new LilachClient("localhost", Constants.SERVER_PORT);
     public static volatile Object msgFromServer = null;
 
     private static Object waitForMsgFromServer() {
