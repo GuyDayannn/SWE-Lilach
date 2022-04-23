d9974ba6abd44f3f480bdb988c3c1a438cf1f9cf
YotamGurfinkel <yotam706@gmail.com>
2022-04-23 19:38:34 +0300

    Started changing Request class to include id

--- entities/src/main/java/org/cshaifa/spring/entities/responses/UpdateItemResponse.java
+++ entities/src/main/java/org/cshaifa/spring/entities/responses/UpdateItemResponse.java
@@ -2,8 +2,8 @@ package org.cshaifa.spring.entities.responses;
 
 public class UpdateItemResponse extends Response {
 
-    public UpdateItemResponse(boolean success) {
-        super(success);
+    public UpdateItemResponse(int requestId, boolean success) {
+        super(requestId, success);
     }
 
 }
