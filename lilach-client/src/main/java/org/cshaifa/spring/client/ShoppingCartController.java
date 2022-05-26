82405accbe8295be1bdaee90b1fe9edc1fa657f5
Ran Toledo <rtoledo_x@hotmail.com>
2022-05-26 15:30:39 +0300

    Started report implementation

--- lilach-client/src/main/java/org/cshaifa/spring/client/ShoppingCartController.java
+++ lilach-client/src/main/java/org/cshaifa/spring/client/ShoppingCartController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
